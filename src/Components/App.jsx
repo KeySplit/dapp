9b50aa21728da7381c8ceaca543f41202b79135b
barrasso <barrassomark@gmail.com>
2018-03-24 11:50:55 -0700

    add back button to header component for relevant routes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
     render() {
         return (
             <div className="main-container">
-                <Header/>
+                <Route path ="*" component={Header}/>
                 <div className="main">
                     <div className="fl">
                         <div className="fl-100">
