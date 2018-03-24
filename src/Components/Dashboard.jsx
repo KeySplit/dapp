9da796645a4679223fc92480a661f6cc7439bd17
barrasso <barrassomark@gmail.com>
2018-03-24 11:23:27 -0700

    key ring empty state implemented

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -11,13 +11,14 @@ class Dashboard extends Component {
         super(props);
         this.state = {
             keys: false,
+            shards: false,
             keyInfo: [],
             keyShards: []
         }
     }
 
     componentWillMount = () => {
-        if (window.web3 !== undefined) {
+        if(window.web3 !== undefined) {
             this.props.ETHaccount().then( (response) => {
                 if(response.account) {
                     if(!(localStorage.getItem(`${response.account}:password`)) && (this.props.location.pathname === "/dashboard")) {
@@ -31,31 +32,35 @@ class Dashboard extends Component {
     }
 
     componentDidMount = () => {
-        if(localStorage.getItem(`${localStorage.account}:pkeys`)){
+        if(localStorage.getItem(`${localStorage.account}:pkeys`)) {
             this.setState({
                 keys: true,
                 keyInfo: JSON.parse(localStorage.getItem(`${localStorage.account}:pkeys`))
             });
         }
-        else{
-            console.log("No keys detected")
-        }
 
-        if(localStorage.getItem(`${localStorage.account}:heldShards`)){
+        if(localStorage.getItem(`${localStorage.account}:heldShards`)) {
             this.setState({
+                shards: true,
                 keyShards: JSON.parse(localStorage.getItem(`${localStorage.account}:heldShards`))
             });
         }
     }
 
     render() {
-        let panel = null;
+        let keysPanel = null;
+        let shardsPanel = null;
         if(!this.state.keys) {
-            panel = <NoKeysPanel history={this.props.history} />
+            keysPanel = <NoKeysPanel history={this.props.history} />
+        } else {
+            keysPanel = <KeysPanel keys={this.state.keyInfo} history={this.props.history} />
         }
-        else{
-            panel = <KeysPanel keys={this.state.keyInfo} history={this.props.history} />
+        if(!this.state.shards) {
+            shardsPanel = <NoShardsPanel history={this.props.history} />
+        } else {
+            shardsPanel = <ShardsPanel shards={this.state.keyShards} history={this.props.history} />
         }
+
         return (
             <div className="dashboard">
                 <Tabs>
@@ -65,21 +70,11 @@ class Dashboard extends Component {
                     </TabList>
 
                     <TabPanel>
-                        {panel}
+                        {keysPanel}
                     </TabPanel>
 
                     <TabPanel>
-                        { this.state.keyShards.map( (shard, index) =>
-                            <div key={index} className="fl-row">
-                                <div className="fl-100">
-                                    <div className="fl-row key-row shard-row">
-                                        <div className="fl-90">
-                                            <span>{shard}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )}
+                        {shardsPanel}
                     </TabPanel>
                 </Tabs>
             </div>
@@ -101,7 +96,6 @@ const mapDispatchToProps = (dispatch) => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
 
-
 class NoKeysPanel extends Component {
     render() {
         return (
@@ -111,7 +105,7 @@ class NoKeysPanel extends Component {
                         <img alt="" src={require("../Assets/images/dashboard/sad_logo.png")} />
                     </div>
                     <div className="fl-70">
-                        <p className="mykeys-par">You have no seed words securely stored with KeySplit.</p>
+                        <p className="mykeys-par">You have no seed words <br/>securely stored with KeySplit.</p>
                         <Link to="">Get started by adding a key below.</Link>
                     </div>
                 </div>
@@ -148,6 +142,47 @@ class KeysPanel extends Component {
                 })}
                 <center><button onClick={() => { this.props.history.push('/add-key/step1') }} className="create-account">ADD KEY</button></center>
             </div>
-        );
+        )
+    }
+}
+
+class NoShardsPanel extends Component {
+    render() {
+        return (
+            <div>
+                <div className="fl-row panel">
+                    <div className="fl-30">
+                        <img alt="" src={require("../Assets/images/dashboard/sad_logo.png")} />
+                    </div>
+                    <div className="fl-70">
+                        <p className="mykeys-par">You aren't guarding <br/>anyone's key shards yet.</p>
+                        <Link to="">Tell your friends they need <br/>to step up their security!</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class ShardsPanel extends Component {
+    render() {
+        const shardData = this.props.shards;
+        return (
+            <div>
+                {shardData.map(function(s, index){
+                    return (
+                        <div key={index} className="fl-row">
+                            <div className="fl-100">
+                                <div className="fl-row key-row shard-row">
+                                    <div className="fl-90">
+                                        <span>{s}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
     }
 }
